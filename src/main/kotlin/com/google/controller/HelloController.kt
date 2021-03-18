5a2b8ab4713bcf9ab1f1ffc30cfd72759cd137be
shinyay <shinya.com@gmail.com>
2021-03-18 11:33:06 +0900

    Add REST Controller Class with Class Level URI

--- src/main/kotlin/com/google/controller/HelloController.kt
+++ src/main/kotlin/com/google/controller/HelloController.kt
@@ -1,7 +1,9 @@
 package com.google.controller
 
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
+@RequestMapping("/api/v1")
 class HelloController {
 }
