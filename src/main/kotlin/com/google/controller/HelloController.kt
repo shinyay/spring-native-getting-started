98e30b4e7cdc220638b541733e9be1e05a4661fd
shinyay <shinya.com@gmail.com>
2021-03-18 11:36:02 +0900

    Add current time function

--- src/main/kotlin/com/google/controller/HelloController.kt
+++ src/main/kotlin/com/google/controller/HelloController.kt
@@ -1,9 +1,22 @@
 package com.google.controller
 
+import com.google.logger
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import java.time.ZoneId
+import java.time.ZonedDateTime
+import java.time.format.DateTimeFormatter
 
 @RestController
 @RequestMapping("/api/v1")
 class HelloController {
+
+    @GetMapping("/hello")
+    fun hello(): String {
+        val dateFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS")
+        val currentTime = ZonedDateTime.now(ZoneId.of("Japan")).format(dateFormatter)
+        logger.info(currentTime)
+        return "Current Timr: $currentTime"
+    }
 }
